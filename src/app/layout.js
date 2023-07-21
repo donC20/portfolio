2d81e5e3a1a2b8d01a8ee1bdf0fc22b2705acc95
don benny <donbenny2024@mca.ajce.in>
2023-07-21 10:16:30 +0530

    metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,10 +5,10 @@ import HeadComponent from '@/components/Head/Head'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './globals.css'
 
-export const metadata = {
-  title: 'Home | Don Benny',
-  description: "Welcome to my developer website, where I showcase my skills and expertise as a software developer. With a passion for crafting efficient and innovative solutions, I specialize in web and mobile application development. From front-end development using modern frameworks to back-end programming with scalable architectures, I bring ideas to life through clean code and intuitive user experiences. Through this website, you can explore my portfolio, featuring a diverse range of projects that highlight my technical prowess and attention to detail. Whether it's building responsive websites, optimizing performance, or integrating APIs, I am dedicated to delivering high- quality solutions tailored to meet your specific needs.Join me on this journey of exploration, collaboration, and innovation, and let's bring your digital vision to reality together.",
-}
+// export const metadata = {
+//   title: 'Home | Don Benny',
+//   description: "Welcome to my developer website, where I showcase my skills and expertise as a software developer. With a passion for crafting efficient and innovative solutions, I specialize in web and mobile application development. From front-end development using modern frameworks to back-end programming with scalable architectures, I bring ideas to life through clean code and intuitive user experiences. Through this website, you can explore my portfolio, featuring a diverse range of projects that highlight my technical prowess and attention to detail. Whether it's building responsive websites, optimizing performance, or integrating APIs, I am dedicated to delivering high- quality solutions tailored to meet your specific needs.Join me on this journey of exploration, collaboration, and innovation, and let's bring your digital vision to reality together.",
+// }
 
 export default function RootLayout({ children }) {
   return (
