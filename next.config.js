3fcbfc0f9d1b4cb474a3c2ea55a65175540ce40f
don benny <donbenny2024@mca.ajce.in>
2023-07-21 10:42:38 +0530

    imges fix

--- next.config.js
+++ next.config.js
@@ -2,7 +2,14 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-    /* config options here */
+    output: 'export',
+    // Optional: Add a trailing slash to all paths `/about` -> `/about/`
+    // trailingSlash: true,
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'dist',
+    images: {
+        unoptimized: true,
+    },
 }
 
 module.exports = nextConfig
